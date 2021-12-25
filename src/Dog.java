e32742e186f70cf8a81d8f60b37b737a21dbce17
JayzzerJK <jayzzers_team@mail.ru>
2021-12-25 18:17:22 +0900

    Initial commit

---
+++ src/Dog.java
@@ -0,0 +1,25 @@
+public class Dog extends Animal {
+    @Override
+    public void run(double distance) {
+        if ((distance > 0)) {
+            System.out.println("true");
+            System.out.println("false");
+        }
+    }
+
+    @Override
+    public void swim(double meters) {
+        if ((meters > 0)) {
+            System.out.println("true");
+            System.out.println("false");
+        }
+    }
+
+    @Override
+    public void jump(double height) {
+        if ((height > 0)) {
+            System.out.println("true");
+            System.out.println("false");
+        }
+    }
+}
